98403837e6347899de173d59e9b1951061549693
piiivot <the_pivotsk8@hotmail.com>
2023-11-08 11:29:35 -0500

    feat created methode get by id in controller

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Jeep'];
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'Jeep'];
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarsById(@Param('id') id: number) {
+    return this.cars[+id];
   }
 }
