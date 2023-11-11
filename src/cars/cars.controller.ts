50f983afafbedcd7f27a60a6a89b34d79d3e31b7
piiivot <the_pivotsk8@hotmail.com>
2023-11-10 20:01:25 -0500

    feat finish CRUD

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -13,6 +13,7 @@ import {
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 // @UsePipes(ValidationPipe)
@@ -31,19 +32,19 @@ export class CarsController {
 
   @Post()
   createCar(@Body() CreateCarDto: CreateCarDto) {
-    return CreateCarDto;
+    return this.carsService.create(CreateCarDto);
   }
 
   @Patch(':id')
-  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
-    return body;
+  updateCar(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCarDto: UpdateCarDto,
+  ) {
+    return this.carsService.update(id, updateCarDto);
   }
 
   @Delete(':id')
-  deleteCar(@Param('id', ParseIntPipe) id: number) {
-    return {
-      methode: 'delete',
-      id,
-    };
+  deleteCar(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.deleted(id);
   }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,7 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 
+// para evitar de tener mil lienas con varios dto's lo que se puede hacer es crear un index en la carp dto y exportar los dtos diredtamente de esta ultima
 import { Car } from './interface/car.interface';
+import { CreateCarDto, UpdateCarDto } from './dto';
 @Injectable()
 export class CarsService {
   private cars: Car[] = [
@@ -32,5 +38,41 @@ export class CarsService {
     if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
 
     return car;
+  }
+
+  create(CreateCarDto: CreateCarDto) {
+    const car: Car = {
+      ...CreateCarDto,
+      id: uuid(),
+    };
+    this.cars.push(car);
+    return car;
+  }
+
+  update(id: string, updateCarDto: UpdateCarDto) {
+    let carDB = this.findOneById(id);
+
+    if (updateCarDto.id && updateCarDto.id !== id) {
+      throw new BadRequestException(`Car id isn't valide inside body`);
+    }
+
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        carDB = {
+          ...carDB,
+          ...updateCarDto,
+          id,
+        };
+        return carDB;
+      }
+      return car;
+    });
+
+    return carDB;
+  }
+
+  deleted(id: string) {
+    this.findOneById(id);
+    this.cars = this.cars.filter((car) => car.id !== id);
   }
 }
